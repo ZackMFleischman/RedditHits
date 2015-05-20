82a9382a8d98e11660bfd269e057d47642109389
zFleischman <zFleischman@gmail.com>
2015-05-19 21:53:04 -0700

    Taking out subreddits for other peoples functionality

---
+++ GetMyHits.py
@@ -0,0 +1,5 @@
+from GetHits import getAllHits
+
+subreddits = [("woahdude",100),("worldnews",1000), ("prorevenge",100), ("funny",3000), ("bestof",400),  ("askreddit",1000), ("explainlikeimfive",300), ("iama",500), ("technology",400), ("wtf",500),("nsfw", 1000), ("gonewild", 1000), ("morbidreality",150)]
+
+getAllHits(subreddits)
